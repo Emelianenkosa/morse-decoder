8a82ac0a1aa2b78af419a08284d27cb4d387e369
Emelianenkosa <emelianenko@tut.by>
2023-01-17 00:49:37 +0300

    feat: task passing

--- src/index.js
+++ src/index.js
@@ -39,6 +39,25 @@ const MORSE_TABLE = {
 
 function decode(expr) {
     // write your solution here
+    let arrayExpr = expr.match(/.{1,10}/g);
+    
+    
+    for(let i=0; i<arrayExpr.length; i++) {
+        arrayExpr[i] = arrayExpr[i].replace(/00/g, '');
+        arrayExpr[i] = arrayExpr[i].replace(/10/g, '.');
+        arrayExpr[i] = arrayExpr[i].replace(/11/g, '-');
+        arrayExpr[i] = arrayExpr[i].replace('**********', 'space');
+    }
+    
+    for(let i=0;i<arrayExpr.length; i++) {
+        if (arrayExpr[i] === 'space') {arrayExpr[i] = ' ' } else
+        arrayExpr[i] = MORSE_TABLE[arrayExpr[i]];
+    };
+
+    let str = arrayExpr.join('');
+    //str = str.replace(/place/g, ' ');
+
+    return str;
 }
 
 module.exports = {
